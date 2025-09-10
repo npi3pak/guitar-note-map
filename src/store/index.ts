a7cef29ed1018303694c5bdd49496bde6a022bb7
Ivan Korablin <ivan.korablin@sensus.aero>
2025-09-10 12:12:20 +0300

    Add fret population

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import * as R from 'ramda';
+import { FULL_NOTES } from 'src/constants';
 
 interface IFret {
     name: string;
@@ -21,19 +22,48 @@ interface IFretBoardActions {
     pressNote: (stringNumber: number, fretNumber: number) => void;
 }
 
+const getNoteListSlice = (initialNote: string, sliceLen: number = 12) =>
+    FULL_NOTES.slice(FULL_NOTES.indexOf(initialNote), FULL_NOTES.indexOf(initialNote) + sliceLen);
+
+const getFretList = (baseNote: string) =>
+    getNoteListSlice(baseNote).reduce((acc, item, index) => {
+        return {
+            ...acc,
+            [index]: {
+                name: item,
+                baseNote: item.replace(/[0-9]/, ''),
+                pressed: false,
+                similar: false,
+            },
+        };
+    }, {});
+
 export const useFretBoardStore = create<IFretBoardState & IFretBoardActions>((set) => {
     return {
         strings: {
             0: {
                 number: 1,
-                fret: {
-                    0: {
-                        name: 'E1',
-                        baseNote: 'E',
-                        pressed: false,
-                        similar: false,
-                    },
-                },
+                fret: getFretList('E4'),
+            },
+            1: {
+                number: 2,
+                fret: getFretList('B3'),
+            },
+            2: {
+                number: 3,
+                fret: getFretList('G3'),
+            },
+            3: {
+                number: 4,
+                fret: getFretList('D3'),
+            },
+            4: {
+                number: 5,
+                fret: getFretList('A2'),
+            },
+            5: {
+                number: 6,
+                fret: getFretList('E2'),
             },
         },
         pressNote: (stringNumber, fretNumber) =>
