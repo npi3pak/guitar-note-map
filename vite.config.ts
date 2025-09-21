a2f21ef331982547396fbc7cdcc652d2d6c6830b
npi3pak <ivan.s.korablin@gmail.com>
2025-09-21 19:31:42 +0300

    Add deploy to pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/guitar-note-map/',
     resolve: {
         alias: {
             src: path.resolve(__dirname, './src'),
