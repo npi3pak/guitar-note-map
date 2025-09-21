cba70da261c18191d41a188694d66cdc85cdbe89
npi3pak <ivan.s.korablin@gmail.com>
2025-09-21 15:41:58 +0300

    Fix piano styles

--- src/components/Piano/index.tsx
+++ src/components/Piano/index.tsx
@@ -1,38 +1,42 @@
 import classnames from 'classnames';
 import { useFretBoardStore } from 'src/store';
 
-const whiteKey = 'relative flex-1 border-l border-gray-400/25 rounded-b-xl hover:cursor-pointer';
-const blackKey = 'absolute top-0 left-[65%] w-[70%] h-[60%] bg-gray-500 z-10 rounded-b-xl hover:cursor-pointer';
+const whiteKeyStyle = 'relative flex-1 border-l border-gray-400/25 rounded-b-xl hover:cursor-pointer';
+const blackKeyStyle = 'absolute top-0 left-[65%] w-[70%] h-[60%] z-10 rounded-b-xl hover:cursor-pointer';
 
 const colorMap: Record<number, string> = {
     1: 'bg-blue-300/25 text-blue-500/50',
-    2: 'bg-red-300/25 text-red-500/50',
+    2: 'bg-(--color-piano-Csharp) text-red-500/50',
     3: 'bg-green-300/25 text-green-500/50',
-    4: 'bg-yellow-300/25 text-yellow-500/50',
+    4: 'bg-(--color-piano-Dsharp) text-yellow-500/50',
     5: 'bg-purple-300/25 text-purple-500/50',
     6: 'bg-pink-300/25 text-pink-500/50',
-    7: 'bg-indigo-300/25 text-indigo-500/50',
+    7: 'bg-(--color-piano-Fsharp) text-indigo-500/50',
     8: 'bg-teal-300/25 text-teal-500/50',
-    9: 'bg-orange-300/25 text-orange-500/50',
+    9: 'bg-(--color-piano-Gsharp) text-orange-500/50',
     10: 'bg-lime-300/25 text-lime-500/50',
-    11: 'bg-cyan-300/25 text-cyan-500/50',
+    11: 'bg-(--color-piano-Asharp) text-cyan-500/50',
     12: 'bg-rose-300/25 text-rose-500/50',
 };
 
-const isHighlightedStyles = (note: string, highlightNotes: any) => {
-    if (!highlightNotes[note].display) {
-        return '';
+const getColor = ({ note, highlightNotes, isPressed = false }) => {
+    const highlightColorNum =
+        highlightNotes[note].hover || highlightNotes[note].display ? colorMap[highlightNotes[note].colorNum] : null;
+    const isBlackKey = note.endsWith('#');
+
+    if (isPressed) {
+        return 'bg-(--color-piano-pressed)';
     }
 
-    return `${colorMap[highlightNotes[note].colorNum]} font-semibold`;
-};
-
-const isHighlightedHoverStyles = (note: string, highlightNotes: any) => {
-    if (!highlightNotes[note].hover) {
-        return '';
+    if (highlightColorNum) {
+        return highlightColorNum;
     }
 
-    return `${colorMap[highlightNotes[note].colorNum]} font-semibold`;
+    if (isBlackKey) {
+        return 'bg-gray-500';
+    }
+
+    return 'bg-(--color-piano-white)';
 };
 
 const Octave = () => {
@@ -42,73 +46,23 @@ const Octave = () => {
 
     return (
         <div className="flex h-48 w-120">
-            <div
-                className={classnames(
-                    `border-l-0 ${whiteKey} ${isHighlightedHoverStyles('C', highlightNotes) || 'bg-white'}`,
-                )}
-                onMouseEnter={() => addHoverNote('C')}
-                onMouseLeave={() => removeHoverNote('C')}
-            >
-                <div
-                    className={classnames(`${blackKey} ${isHighlightedHoverStyles('C#', highlightNotes)}`)}
-                    onMouseEnter={() => addHoverNote('C#')}
-                    onMouseLeave={() => removeHoverNote('C#')}
-                ></div>
+            <div className={classnames(`border-l-0 ${whiteKeyStyle} ${getColor({ note: 'C', highlightNotes })}`)}>
+                <div className={classnames(`${blackKeyStyle} ${getColor({ note: 'C#', highlightNotes })}`)}></div>
             </div>
-            <div
-                className={classnames(`${whiteKey} ${isHighlightedHoverStyles('D', highlightNotes) || 'bg-white'}`)}
-                onMouseEnter={() => addHoverNote('D')}
-                onMouseLeave={() => removeHoverNote('D')}
-            >
-                <div
-                    className={classnames(`${blackKey} ${isHighlightedHoverStyles('D#', highlightNotes)}`)}
-                    onMouseEnter={() => addHoverNote('D#')}
-                    onMouseLeave={() => removeHoverNote('D#')}
-                ></div>
+            <div className={classnames(`${whiteKeyStyle} ${getColor({ note: 'D', highlightNotes })}`)}>
+                <div className={classnames(`${blackKeyStyle} ${getColor({ note: 'D#', highlightNotes })}`)}></div>
             </div>
-            <div
-                className={classnames(`${whiteKey} ${isHighlightedHoverStyles('E', highlightNotes) || 'bg-white'}`)}
-                onMouseEnter={() => addHoverNote('E')}
-                onMouseLeave={() => removeHoverNote('E')}
-            ></div>
-            <div
-                className={classnames(`${whiteKey} ${isHighlightedHoverStyles('F', highlightNotes) || 'bg-white'}`)}
-                onMouseEnter={() => addHoverNote('F')}
-                onMouseLeave={() => removeHoverNote('F')}
-            >
-                <div
-                    className={classnames(`${blackKey} ${isHighlightedHoverStyles('F#', highlightNotes)}`)}
-                    onMouseEnter={() => addHoverNote('F#')}
-                    onMouseLeave={() => removeHoverNote('F#')}
-                ></div>
+            <div className={classnames(`${whiteKeyStyle} ${getColor({ note: 'E', highlightNotes })}`)}></div>
+            <div className={classnames(`${whiteKeyStyle} ${getColor({ note: 'F', highlightNotes })}`)}>
+                <div className={classnames(`${blackKeyStyle} ${getColor({ note: 'F#', highlightNotes })}`)}></div>
             </div>
-            <div
-                className={classnames(`${whiteKey} ${isHighlightedHoverStyles('G', highlightNotes) || 'bg-white'}`)}
-                onMouseEnter={() => addHoverNote('G')}
-                onMouseLeave={() => removeHoverNote('G')}
-            >
-                <div
-                    className={classnames(`${blackKey} ${isHighlightedHoverStyles('G#', highlightNotes)}`)}
-                    onMouseEnter={() => addHoverNote('G#')}
-                    onMouseLeave={() => removeHoverNote('G#')}
-                ></div>
+            <div className={classnames(`${whiteKeyStyle} ${getColor({ note: 'G', highlightNotes })}`)}>
+                <div className={classnames(`${blackKeyStyle} ${getColor({ note: 'G#', highlightNotes })}`)}></div>
             </div>
-            <div
-                className={classnames(`${whiteKey} ${isHighlightedHoverStyles('A', highlightNotes) || 'bg-white'}`)}
-                onMouseEnter={() => addHoverNote('A')}
-                onMouseLeave={() => removeHoverNote('A')}
-            >
-                <div
-                    className={classnames(`${blackKey} ${isHighlightedHoverStyles('A#', highlightNotes)}`)}
-                    onMouseEnter={() => addHoverNote('A#')}
-                    onMouseLeave={() => removeHoverNote('A#')}
-                ></div>
+            <div className={classnames(`${whiteKeyStyle} ${getColor({ note: 'A', highlightNotes })}`)}>
+                <div className={classnames(`${blackKeyStyle} ${getColor({ note: 'A#', highlightNotes })}`)}></div>
             </div>
-            <div
-                className={classnames(`${whiteKey} ${isHighlightedHoverStyles('B', highlightNotes) || 'bg-white'}`)}
-                onMouseEnter={() => addHoverNote('B')}
-                onMouseLeave={() => removeHoverNote('B')}
-            ></div>
+            <div className={classnames(`${whiteKeyStyle} ${getColor({ note: 'B', highlightNotes })}`)}></div>
         </div>
     );
 };
