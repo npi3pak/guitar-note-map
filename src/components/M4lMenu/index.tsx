89c20f8f4227ec04d800c2fbb535d16e97d85225
Ivan Korablin <ivan.korablin@sensus.aero>
2025-10-06 10:31:26 +0300

    fix

--- src/components/M4lMenu/index.tsx
+++ src/components/M4lMenu/index.tsx
@@ -15,37 +15,36 @@ export const M4lMenu = () => {
 
     return (
         <div className="p-2 pt-6 flex">
-            <div className="p-1">
-                <div className="flex flex-col px-4 py-1">
-                    <button
-                        className="btn btn-xs btn-outline border-ableton-orange text-ableton-orange my-1"
-                        onClick={() => setLock()}
-                    >
+            <div className="p-3 flex flex-col bg-ableton-black card  w-[150px]">
+                <div className="flex flex-row mt-2 justify-items-center items-center">
+                    <button className="border-ableton-orange text-ableton-orange mr-1" onClick={() => setLock()}>
                         {isLocked ? lockIcon : unlockIcon}
                     </button>
+
                     <button
-                        className="btn btn-xs btn-outline border-ableton-orange text-ableton-orange my-1"
+                        className="border-ableton-orange text-ableton-orange mr-1 text-[1px]"
                         onClick={() => tuneDownAll()}
                         disabled={isLocked}
                     >
                         {chevronLeft}
                     </button>
+                    <label className="text-ableton-orange text-[10px]">fred tuning</label>
                     <button
-                        className="btn btn-xs btn-outline border-ableton-orange text-ableton-orange my-1"
+                        className="border-ableton-orange text-ableton-orange mr-1 text-[1px]"
                         onClick={() => tuneUpAll()}
                         disabled={isLocked}
                     >
                         {chevronRight}
                     </button>
-                    {!isLocked && (
-                        <button
-                            className="btn btn-xs btn-outline border-ableton-orange text-ableton-orange my-1"
-                            onClick={() => tuneToStandard()}
-                            disabled={isLocked}
-                        >
-                            Reset to standart
-                        </button>
-                    )}
+                </div>
+                <div className="flex flex-row">
+                    <button
+                        className="border-ableton-orange text-ableton-orange my-1 text-[8px]"
+                        onClick={() => tuneToStandard()}
+                        disabled={isLocked}
+                    >
+                        Reset to standard
+                    </button>
                 </div>
             </div>
         </div>
