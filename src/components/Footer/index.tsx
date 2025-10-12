9bfc0e38da21a182271e25c07a411670a5b5d94d
npi3pak <ivan.s.korablin@gmail.com>
2025-10-12 23:25:56 +0300

    Add botton link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,8 +2,16 @@ import { Github } from 'lucide-react';
 
 export const Footer = () => {
     return (
-        <footer className="footer p-4 text-gray-800/25 text-center justify-center">
-            <div className="flex items-center justify-center gap-2">
+        <footer className="footer p-4 text-gray-800/25 text-center justify-around">
+            <div className="flex items-center justify-center gap-2 mx-4">
+                <a
+                    href="https://acidvoid.gumroad.com/l/guitar-note-map"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-1"
+                >
+                    <span className="hover:text-red-300 bold text-red-400">Try Max For Live Plugin</span>
+                </a>
                 <span>Ivan Void 2025</span>
                 <a
                     href="https://github.com/npi3pak"
