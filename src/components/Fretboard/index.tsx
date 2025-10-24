75569845b4e9b03f773809fa1bc202bb6f840617
npi3pak <ivan.s.korablin@gmail.com>
2025-10-24 21:17:48 +0300

    transform styles

--- src/components/Fretboard/index.tsx
+++ src/components/Fretboard/index.tsx
@@ -3,12 +3,32 @@ import { String } from 'components/String';
 import { StringsTuneShift } from 'src/components/Fretboard/StringsTuneShift';
 import { StringsCountOptions } from 'components/Fretboard/StringsCountOptions';
 import { useFretBoardStore } from 'src/store';
-import styles from './styles.module.css';
+import appStyles from './appStyles.module.css';
+import m4lStyles from './m4lStyles.module.css';
 import React from 'react';
 
 interface IProps {
     m4l?: boolean;
 }
+
+const FretTopNum = () => (
+    <>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((index) => {
+            const isMarker = [3, 5, 7, 9].includes(index);
+
+            return (
+                <div
+                    key={index}
+                    className={classnames(appStyles.fretMarkers, {
+                        [appStyles.fretMarkersRound]: isMarker,
+                    })}
+                >
+                    {index}
+                </div>
+            );
+        })}
+    </>
+);
 
 export const Fretboard: React.FC<IProps> = ({ m4l = false }) => {
     const { getStringsCount, resetPressedNotes } = useFretBoardStore();
@@ -20,26 +40,10 @@ export const Fretboard: React.FC<IProps> = ({ m4l = false }) => {
 
     if (m4l) {
         return (
-            <div className="flex overflow-x-auto h-[165px] mt-1">
-                <div className={styles['fretboard-m4l']}>
+            <div className={m4lStyles.fretboardContainer}>
+                <div className={m4lStyles.fretboard}>
                     <div />
-                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((index) => {
-                        const isMarker = [3, 5, 7, 9].includes(index);
-
-                        return (
-                            <div
-                                key={index}
-                                className={classnames(
-                                    'flex items-center justify-center self-center justify-self-center text-yellow-500 font-bold text-xs',
-                                    {
-                                        'rounded-full w-5 h-5 bg-gray-600/20': isMarker,
-                                    },
-                                )}
-                            >
-                                {index}
-                            </div>
-                        );
-                    })}
+                    <FretTopNum />
                     {[...Array(stringsCount).keys()].map((stringNum, item) => (
                         <String stringNumber={stringNum + 1} key={item} m4l />
                     ))}
@@ -49,27 +53,11 @@ export const Fretboard: React.FC<IProps> = ({ m4l = false }) => {
     }
 
     return (
-        <div className="px-4 md:px-12 pt-6">
+        <div className={appStyles.fretboardContainer}>
             <div className="bg-base-100 rounded-box p-4 overflow-x-auto card card-border border-base-300">
-                <div className={styles.fretboard}>
+                <div className={appStyles.fretboard}>
                     <StringsCountOptions />
-                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((index) => {
-                        const isMarker = [3, 5, 7, 9].includes(index);
-
-                        return (
-                            <div
-                                key={index}
-                                className={classnames(
-                                    'flex items-center justify-center self-center justify-self-center text-red-500/50 font-bold',
-                                    {
-                                        'rounded-full w-6 h-6 bg-blue-200/50': isMarker,
-                                    },
-                                )}
-                            >
-                                {index}
-                            </div>
-                        );
-                    })}
+                    <FretTopNum />
                     {[...Array(stringsCount).keys()].map((stringNum, item) => (
                         <String stringNumber={stringNum + 1} key={item} />
                     ))}
